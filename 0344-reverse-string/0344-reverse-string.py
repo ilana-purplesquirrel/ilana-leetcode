714ca598001ff7b2d6c8f79dc6d7ba7d053d3d8a
ilana-logalt <pivotaltarot@gmail.com>
2025-04-14 13:14:34 -0700

    initial solution for 0344, reverse string

---
+++ 0344-reverse-string/0344-reverse-string.py
@@ -0,0 +1,25 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        
+        l = len(s)
+        if l==1 return s
+
+        ia = 0
+        ib = l - 1
+
+        while ia < ib:
+            temp = s[ia]
+            s[ia] = s[ib]
+            s[ib] = temp
+
+            ia += 1
+            ib -= 1
+        
+        return s
+    
+        # time complexity: O(n)
+        # space complexity: O(1)
+        
