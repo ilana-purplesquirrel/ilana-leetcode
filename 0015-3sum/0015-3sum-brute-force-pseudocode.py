edacabded50ed9edd4e0e5d89305e9c0e89dff0a
ilana-logalt <pivotaltarot@gmail.com>
2025-04-28 15:09:29 -0700

    adding in-class pseudocode for an O(n^3) brute force solution

---
+++ 0015-3sum/0015-3sum-brute-force-pseudocode.py
@@ -0,0 +1,17 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+    
+    # three pointers
+    # walk the pointers through the input,
+    # generating all possible triplets from the input:
+    # three nested loops:
+    # i,j,k
+    # time complexity O(n^3):
+    # for i from 0 to len(num)-3
+    #       for j from i+1 to len(num)-2
+    #           for k from j+1 to len(num)-1
+
+
+                    # check the sum of each triplet and save unique triplets that have added to zero
+
+                    # TODO: check for uniqueness
