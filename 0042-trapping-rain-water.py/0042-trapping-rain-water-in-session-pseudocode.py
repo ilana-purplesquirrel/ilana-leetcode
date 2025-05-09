9558e4652f99b62422e12f16fbe6fad9866fb381
ilana-logalt <pivotaltarot@gmail.com>
2025-05-09 15:32:22 -0700

    adding NeetCode solutions and in-class pseudocode for 0045, trapping rain water

---
+++ 0042-trapping-rain-water.py/0042-trapping-rain-water-in-session-pseudocode.py
@@ -0,0 +1,36 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        
+        # edge cases:
+        # if length of input < 3, return 0
+
+        # assume length of 3 or greater
+
+        # max_height = max(height)
+
+        # water_found = 0
+
+        # do the things :)
+
+        # two loops:
+    
+    
+        # moving top to bottom:
+        # for row in (max_height downto 0)
+        #   sweeping left to right:
+        #   bool: in_water = False
+        #   bool: in_wall = False
+        #   cur_left_wall = 0
+        #   cur_right_wall = len(height)-1
+        #   for col in range(0, len(height)):
+        #       if (not in_water) and (not in_wall):
+        #           if height[col] >= row:
+        #           in_wall = True
+        #           cur_left_wall = col
+        #       
+
+    
+
+        # return water_count
+
+        
